442bb44ce27adf4505cfb6ef53b180a97b8353cf
Youssef <125336900+yag1Rice@users.noreply.github.com>
2024-08-26 20:59:35 -0500

    Update job.sh

--- Server/job.sh
+++ Server/job.sh
@@ -4,8 +4,8 @@
 
 #pwd
 
-nvidia-smi
+nvidia-smi #Checks the number of gpus alloted & CUDA driver version
 
-cd summer_research
+cd summer_research #replace with name of directory where python file to be executed
 
-python rescale.py
+python rescale.py #replace with name of python file to be executed
