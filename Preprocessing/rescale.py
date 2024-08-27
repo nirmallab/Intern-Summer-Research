4d7ac2f06841f22808b26b54d06e7319d2764332
Youssef <125336900+yag1Rice@users.noreply.github.com>
2024-08-26 22:09:00 -0500

    Update rescale.py

--- Preprocessing/rescale.py
+++ Preprocessing/rescale.py
@@ -4,7 +4,7 @@ from skimage import io, transform
 # Load the RGB image
 image = io.imread('necrosis_muscle.tif')
 
-rescaled_image = transform.rescale(image, scale=0.65, anti_aliasing=True, channel_axis = 2)
+rescaled_image = transform.rescale(image, scale=0.65, anti_aliasing=True, channel_axis = 2)  #can change scale to fit specefic zoom level/dimension
 
 io.imsave('rescaled.tif', rescaled_image)
 
